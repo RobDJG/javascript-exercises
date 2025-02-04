6d5023c1e787ec9694a1725c6d8eea70681fbfae
Roberto Genao <139014779+RobDJG@users.noreply.github.com>
2025-02-04 08:42:40 -0400

    include solution for problem 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...itemsToRemove) {
+
+    let result = Array.from(array)
+
+    for (let element of itemsToRemove) {
+
+        let removeIndex = result.indexOf(element);
+
+        if (removeIndex === -1) {
+            continue
+        }
+
+        while (removeIndex != -1) {
+
+            result.splice(removeIndex, 1)
+
+            removeIndex = result.indexOf(element);
+
+        }
+
+
+    }
+
+    return result;
+
 };
 
 // Do not edit below this line
