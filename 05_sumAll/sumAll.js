4cd199a670593b6ae4fe1a5e1cc285a9f9d3130e
Roberto Genao <139014779+RobDJG@users.noreply.github.com>
2025-02-05 08:03:53 -0400

    add solution for problem 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function (numA, numB) {
+
+    let result = 0;
+
+    if (numA > numB) {
+        numB = [numA, numA = numB][0];
+    }
+
+    else if ((numA < 0 || numB < 0)
+        || !Number.isInteger(numA)
+        || !Number.isInteger(numB)) {
+        return "ERROR"
+    }
+
+    for (let i = numA; i <= numB; i++) {
+        result += i;
+    }
+
+    return result;
 
 };
 
