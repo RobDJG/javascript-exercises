0672e6ce408398b22438ac437c763b350929cd4c
Roberto Genao <139014779+RobDJG@users.noreply.github.com>
2025-02-05 09:51:07 -0400

    add solution for problem 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,58 @@
-const add = function() {
+const add = function(a, b) {
+
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+
+  let result = 0;
+
+
+  for (let num of array){
+
+    result += Number(num);
+
+  }
+
+  return result;
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+
+  let result = 1;
+
+
+  for (let num of array){
+
+    result *= Number(num);
+
+  }
+
+  return result;
 
 };
 
-const power = function() {
+const power = function(base, exponent) {
+
+  return base ** exponent;
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+
+  if (num == 0) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
 	
 };
 
