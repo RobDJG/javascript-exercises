0f220419824f14b35096e8aff74380176cf612fc
Roberto Genao <139014779+RobDJG@users.noreply.github.com>
2025-02-06 09:33:18 -0400

    add solution to problem 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,34 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+
+    if (num < 0) {
+        return "OOPS"
+    }
+
+    else if (num == 0) {
+        return 0
+    }
+
+    else {
+
+        let previousNum = 0;
+        let result = 1;
+        let temp;
+        num = Number(num)
+
+        for (let i = 1; i < num; i++) {
+
+            temp = result;
+
+            result += previousNum;
+
+            previousNum = temp;
+
+
+        }
+
+        return result;
+
+    }
 
 };
 
