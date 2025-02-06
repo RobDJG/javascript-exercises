1501181105e6955d7a075ddb96cc5eaff5d73096
Roberto Genao <139014779+RobDJG@users.noreply.github.com>
2025-02-06 07:50:17 -0400

    add solution for problem 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,29 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    punctuationArray = ["!", ".", ",", " "]
+
+    originalString = string.toLowerCase()
+
+
+
+    for (let i = 0; i < punctuationArray.length; i++) {
+
+        if (originalString.includes(punctuationArray[i])) {
+            originalString = originalString.replaceAll(punctuationArray[i], "");
+        }
+
+    }
+
+    reversedString = originalString.split("").reverse().join("").toLowerCase()
+
+
+    if (reversedString === originalString) {
+        return true;
+    }
+
+    else {
+        return false;
+    }
 
 };
 
