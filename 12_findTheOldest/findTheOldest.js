2cfd84b1b6a2b084cc3235bf6b97776c6eb81e49
Roberto Genao <139014779+RobDJG@users.noreply.github.com>
2025-02-11 07:59:17 -0400

    add solution for problem 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+
+    let oldest = { age: 0 };
+
+
+
+    for (let i = 0; i < array.length; i++) {
+
+
+        if (!("yearOfDeath" in array[i])) {
+
+            array[i].age = new Date().getFullYear() - array[i].yearOfBirth;
+
+        }
+
+        else {
+            array[i].age = array[i].yearOfDeath - array[i].yearOfBirth
+        }
+
+        if (array[i].age > oldest.age) {
+
+            oldest = array[i];
+
+        }
+
+    }
+
+
+    return oldest;
 
 };
 
