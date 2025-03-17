57e375bfe985d6b3ea41e7f51e773fb9a64f2054
Roberto Genao <139014779+RobDJG@users.noreply.github.com>
2025-03-17 09:40:53 -0400

    include camelize function solution

---
+++ array methods/Javascript info - Excercises/javascript.js
@@ -0,0 +1,13 @@
+function camelize(str) {
+    return str
+        .split("-")
+        .map((item, index) => {
+            if (index === 0) {
+                return item;
+            }
+
+            return item.charAt(0).toUpperCase() + item.slice(1);
+        })
+        .join("")
+}
+
