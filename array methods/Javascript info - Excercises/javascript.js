1b5d120e09c75298cb0511cc868a4c0b007dffd7
Roberto Genao <139014779+RobDJG@users.noreply.github.com>
2025-03-18 10:16:33 -0400

    include function and solution unique

--- array methods/Javascript info - Excercises/javascript.js
+++ array methods/Javascript info - Excercises/javascript.js
@@ -70,3 +70,22 @@ function getAverageAge(users) {
 
 }
 
+function unique(arr) {
+    
+    let result = [];
+
+    for (let i = 0; i < arr.length ; i++){
+        if (!(result.includes(arr[i]))){
+            result.push(arr[i])
+        }
+    }
+
+    return result;
+
+}
+
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+  ];
+
