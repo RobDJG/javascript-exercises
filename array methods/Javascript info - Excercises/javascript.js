89ed944e6fb1dc67b2cbfc5ac19b8a6495a351f6
Roberto Genao <139014779+RobDJG@users.noreply.github.com>
2025-03-18 09:25:39 -0400

    add function and solution for shuffle
    I didn't do the algorithm, i just google how to randomize an array
    and the Fisher-Yates algorithm appeared as de facto randomizer for arrays.

--- array methods/Javascript info - Excercises/javascript.js
+++ array methods/Javascript info - Excercises/javascript.js
@@ -45,3 +45,22 @@ function sortByAge(users) {
     users.sort((a, b) => a.age - b.age);
 
 }
+
+function shuffle(array) {
+    let currentIndex = array.length;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    console.log(array)
+  }
+  
