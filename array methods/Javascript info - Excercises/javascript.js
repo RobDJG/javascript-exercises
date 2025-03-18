265af70a5566ba2b368627ebaa21a29b5173ee88
Roberto Genao <139014779+RobDJG@users.noreply.github.com>
2025-03-18 08:27:25 -0400

    add filterRangeInPlace solution

--- array methods/Javascript info - Excercises/javascript.js
+++ array methods/Javascript info - Excercises/javascript.js
@@ -3,15 +3,31 @@ function camelize(str) {
         .split("-")
         .map(
             (word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1)
-            )
+        )
         .join("");
 }
 
 
 function filterRange(arr, a, b) {
-    
 
     return arr.filter((num) => (num >= a) && (num <= b));
 
+}
+
+function filterRangeInPlace(arr, a, b) {
+
+    for (let i = 0; i < arr.length; i++) {
+
+
+        if (!(a <= arr[i] && arr[i] <= b)) {
+            console.log(`${arr[i]} removed.`)
+            arr.splice(i, 1);
+            i--;
+
+        }
+
+    }
+
+    console.log("hello from outside the loop")
 }
 
